b7ba0352c988c513b3b5290a1cb601f6aa01d2ac
vishal25061997 <vishalyadav.unique2@gmail.com>
2023-07-06 18:41:36 +0530

    Navbar

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
+import styled from "styled-components";
 
 
 const Home =()=>{
-    return <div>Home</div>
+    return <Wrapper  className="test">Home</Wrapper>
 }
 
+const Wrapper = styled.section`
+height: 100vh;
+background-color: ${({theme})=> theme.colors.bg}
+`
 
 export default Home;
