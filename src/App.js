f1e4fa063562f3a76ffe633da918cc058c4b5a45
vishal25061997 <vishalyadav.unique2@gmail.com>
2023-07-06 13:56:44 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Products from "./Products";
+import SingleProduct from "./SingleProduct";
+import Cart from "./Cart";
+import ErroPage from "./ErrorPage";
 
 const App = () => {
-  return <div>Welcome to Thapa React E-Commerce Website</div>;
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/products" element={<Products/>}/>
+      <Route path="/singleProduct" element={<SingleProduct/>}/>
+      <Route path= "/cart"  element={<Cart/>}/>
+      <Route path="*" element ={<ErroPage/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
 };
 
 export default App;
---
+++ src/ErrorPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ErroPage=()=>{
+    return <div>Error</div>
+}
+
+
+export default ErroPage;
+
