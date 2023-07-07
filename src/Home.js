1e955d5d0b0bf08b283e454e3c20c412fb7d6cee
vishal25061997 <vishalyadav.unique2@gmail.com>
2023-07-07 16:04:54 +0530

    home-page

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,10 @@
 import React from "react";
-import styled from "styled-components";
+import HeroSection from "./components/HeroSection";
 
 
 const Home =()=>{
-    return <Wrapper  className="test">Home</Wrapper>
+    return <HeroSection/>
 }
 
-const Wrapper = styled.section`
-height: 100vh;
-background-color: ${({theme})=> theme.colors.bg}
-`
 
 export default Home;
