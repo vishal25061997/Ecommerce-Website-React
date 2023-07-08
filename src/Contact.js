114fc6cd26fa20b6e2a98111333cec04e83cc449
vishal25061997 <vishalyadav.unique2@gmail.com>
2023-07-08 16:22:53 +0530

    contact page

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,9 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import styled from "styled-components";
 
 const Contact = () => {
+  const { isAuthenticated, user } = useAuth0();
+
   const Wrapper = styled.section`
     padding: 9rem 0 5rem 0;
     text-align: center;
@@ -33,7 +36,50 @@ const Contact = () => {
     }
   `;
 
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Contact page</h2>
+
+      
+
+      <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/xnqkqddv"
+            method="POST"
+            className="contact-inputs">
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              value={isAuthenticated ? user.name : ""}
+              required
+              autoComplete="off"
+            />
+
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoComplete="off"
+              value={isAuthenticated ? user.email : ""}
+              required
+            />
+
+            <textarea
+              name="Message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+              placeholder="Enter you message"></textarea>
+
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Contact;
